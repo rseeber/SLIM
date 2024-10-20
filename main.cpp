4f985f184d11a1fa630302482571d82060ef6f6d
River Seeber <riverseeber12@gmail.com>
2024-10-19 18:22:51 -0700

    added initDB() to load database into ram

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <list>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -10,10 +12,43 @@ int editUser(string user, string oldPass, string newPass);
 string loginAsUser(string user, string passwd);
 //write the requested data to the buffer pointed to by buf
 int getUserData(string user, string cookie, int dataID, string* buf);
+//load the database into ram as an array of structs
+int initDB();
+
+//UTILITY FUNCTIONS
 //hashes the passwd with the salt, storing it at the buffer pointed to by buf
 int hashPasswd(string passwd, string salt, string* buf);
 
 
-int addUser(string user, string email, string passwd){
-    
+struct login {
+    string user;
+    string email;
+    string passHash;
+    string salt;
+};
+
+//this is where we load the users.txt data into
+list<login> myLogins;
+
+int main(){
+    initDB();
+    //after init, the user can choose which actions they want to take
+}
+
+//loads the database into ram as a list of login structs
+int initDB(){
+    string line;
+    ifstream ifs("users.txt");
+    //save the current line of the document to `line`
+    for(int i = 0; ifs.good(); ++i){
+        login l;
+        //now break up the line into valid pieces to save as each part of the login
+        ifs >> l.user >> l.email >> l.passHash >> l.salt;
+
+        //append l to myLogins
+        myLogins.push_back(l);
+    }
+    ifs.close();
+
+    return 0;
 }
