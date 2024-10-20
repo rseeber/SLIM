b2c3f7dc9ad1e394b39a2fd7c8685faf58f05c49
River Seeber <riverseeber12@gmail.com>
2024-10-19 19:37:59 -0700

    basic database functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <fstream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 //creates a new user in the database
@@ -14,10 +15,16 @@ string loginAsUser(string user, string passwd);
 int getUserData(string user, string cookie, int dataID, string* buf);
 //load the database into ram as an array of structs
 int initDB();
+//literally just print the whole database
+void printDB();
+//saves the database back into users.txt
+void saveDB();
 
 //UTILITY FUNCTIONS
 //hashes the passwd with the salt, storing it at the buffer pointed to by buf
 int hashPasswd(string passwd, string salt, string* buf);
+//hashes passwd, generating a new salt stored at *salt. Hash is stored at *buf.
+int hashPasswd(string passwd, string* salt, string* buf);
 
 
 struct login {
@@ -33,6 +40,16 @@ list<login> myLogins;
 int main(){
     initDB();
     //after init, the user can choose which actions they want to take
+    addUser("James", "jj-jamison@gmail.com", "reallybadPasswd");
+    printDB();
+    saveDB();
+}
+
+void printDB(){
+    int i = 0;
+    for(login l : myLogins){
+        cout << "[" << i++ << "]: " << l.user << "\t\t" << l.email << "\t\t" << l.passHash << "\t\t" << l.salt << endl;
+    }
 }
 
 //loads the database into ram as a list of login structs
@@ -51,4 +68,56 @@ int initDB(){
     ifs.close();
 
     return 0;
+}
+
+void saveDB(){
+    ofstream ofs("users.txt");
+    for(login l : myLogins){
+        //write the data from the list of structs into the text file
+        ofs << l.user << "\t" << l.email << "\t" << l.passHash << "\t" << l.salt;
+        
+        //only print a newline if we're NOT on the last entry
+        if(myLogins.back().user != l.user){   //compare the usernames
+            ofs << endl;
+        }
+    }
+}
+
+
+
+int addUser(string user, string email, string passwd){
+    login l;
+    l.user = user;
+
+    //first check that this user does not already exist in the system
+    if(binary_search(myLogins.begin(), myLogins.end(), l)){
+        return -1;
+    }
+    
+    //otherwise, proceed
+    l.email = email;
+    hashPasswd(passwd, &l.salt, &l.passHash);
+    //append l to myLogins
+    myLogins.push_back(l);
+    //sort myLogins
+    myLogins.sort();
+    return 0;
+}
+
+int hashPasswd(string passwd, string salt, string* buf){
+    buf->assign("thisisabadhash");
+    return 0;
+}
+
+int hashPasswd(string passwd, string* salt, string* buf){
+    salt->assign("~~badSalt~~");
+    return hashPasswd(passwd, *salt, buf);
+}
+
+bool operator<(const login& first, const login& second){
+    return (first.user < second.user) ? true : false;
+}
+
+bool operator==(const login& a, const login& b){
+    return (a.user == b.user) ? true : false;
 }
